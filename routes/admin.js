f2c93e8fc6793fb062925d7346b3f12399c2ed95
Tanmay Yadav <tanmayyadav2323@gmail.com>
2023-04-27 23:32:42 +0530

    admin updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const admin = require('../middlewares/admin');
 const adminRouter = express.Router();
-
-const {Product} = require('../models/product');
+const Order = require('../models/order');
+const { Product } = require('../models/product');
 
 //Add Product
 adminRouter.post('/admin/add-product', admin, async (req, res) => {
@@ -38,6 +38,39 @@ adminRouter.get('/admin/get-products', admin, async (req, res) => {
     }
 });
 
+adminRouter.get("/admin/analytics", admin, async (req, res) => {
+    try {
+        const orders = await Order.find();
+        let totalEarnings = 0;
+
+        for (let i = 0; i < orders.length; i++) {
+            for (let j = 0; j < orders[i].products.length; j++) {
+                totalEarnings +=
+                    orders[i].products[j].quantity * orders[i].products[j].product.price;
+            }
+        }
+        // CATEGORY WISE ORDER FETCHING
+        let containerEarnings = await fetchCategoryWiseProduct("Containers");
+        let skinCareEarnings = await fetchCategoryWiseProduct("SkinCares");
+        let homegoodEarnings = await fetchCategoryWiseProduct("HomeGoods");
+        let PackagingsEarnings = await fetchCategoryWiseProduct("Packagings");
+        let fashionEarnings = await fetchCategoryWiseProduct("Fashion");
+
+        let earnings = {
+            totalEarnings,
+            containerEarnings,
+            skinCareEarnings,
+            homegoodEarnings,
+            PackagingsEarnings,
+            fashionEarnings,
+        };
+
+        res.json(earnings);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
 //delete the product
 adminRouter.post('/admin/delete-product', admin, async (req, res) => {
     try {
@@ -49,6 +82,22 @@ adminRouter.post('/admin/delete-product', admin, async (req, res) => {
         res.status(500).json({ error: e.message });
     }
 });
+
+async function fetchCategoryWiseProduct(category) {
+    let earnings = 0;
+    let categoryOrders = await Order.find({
+        "products.product.category": category,
+    });
+
+    for (let i = 0; i < categoryOrders.length; i++) {
+        for (let j = 0; j < categoryOrders[i].products.length; j++) {
+            earnings +=
+                categoryOrders[i].products[j].quantity *
+                categoryOrders[i].products[j].product.price;
+        }
+    }
+    return earnings;
+}
 
 
 module.exports = adminRouter;
